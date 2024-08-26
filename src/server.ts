6b927f78ac8c0497152cf5ac879d75a4eade40a4
Gustavo Maia <ms.gustavo@outlook.com>
2024-08-26 10:13:24 -0300

    auth route - register and login controllers

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -8,9 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
