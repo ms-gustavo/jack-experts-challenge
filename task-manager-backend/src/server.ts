4aece6aee992c610eb3b05d93e257e065134e595
Gustavo Maia <ms.gustavo@outlook.com>
2024-08-27 11:49:08 -0300

    cors

--- task-manager-backend/src/server.ts
+++ task-manager-backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import taskRoutes from "./routes/taskRoutes";
 import { setupSwagger } from "./swagger";
@@ -7,7 +8,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 setupSwagger(app);
 app.use("/api/auth", authRoutes);
